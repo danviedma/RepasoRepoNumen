3e967696d7205ad411a18898924f6baf43331f1d
Alejandro Egaña <danviedma@gmail.com>
2023-10-28 19:54:32 -0300

    Se agrega next js

---
+++ primer-next/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Ejemplo from "./components/Ejemplo";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Primer Proyecto</p>
+      <Ejemplo />
+    </div>
+  );
+}
